26ea67a331b824e0af16da10f87854bbd78b9b7d
Samu <csonka.samuel@gmail.com>
2023-10-21 14:52:38 +0200

    hello world done

--- rust-basics/src/main.rs
+++ rust-basics/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+// 1. Hello World
+
+// define a function called "hello_world with no parameters"
+fn hello_world() { 
+    // print out "Hello World!" in a new line
+    println!("Hello World!");
 }
