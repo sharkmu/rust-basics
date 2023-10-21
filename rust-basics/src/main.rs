51258b9794bb327e45a229513ed5107b4d9146c6
Samu <csonka.samuel@gmail.com>
2023-10-21 15:24:04 +0200

    math - subtraction and devision, small changes

--- rust-basics/src/main.rs
+++ rust-basics/src/main.rs
@@ -28,7 +28,7 @@ fn input()
 
 fn math()
 {
-    // addition
+    // addition and subtraction
 
     // define "a", "b" variables and give them integer as value
     let a = 1;
@@ -37,8 +37,34 @@ fn math()
     // give "c" variable the sum of "a" and "b" as value
     let c = a + b;
 
-    // print out "c"
-    println!("{}", c);
+    // give "d" variable the subtract of "b" and "a" as value
+    let d = b - a;
+
+    // print out "c" and "d"
+    println!("Sum: {}", c);
+    println!("Subtraction: {}", d);
+
+    // another way of getting the sum of "a" and "b" 
+    println!("{}", a + b);
+
+    // another way of getting the subtract of "b" and "a"
+    println!("{}", b - a);
+
+    // devision and multiplication
+
+    // f64 means that it is a 64-bit floating-point number. If we want to make it a 32-bit, then just replace "64" with "32"
+    let x = 2f64;
+    let y = 3f64;
+
+    // give "z" variable the product of "x" and "y"
+    let z = x * y;
+
+    // give "w" variable the devision of "y" and "x"
+    let w = y / x;
+
+    // print out "z" and "w"
+    println!("Product: {}", z);
+    println!("Devision: {}", w);
 }
 
 // this is the main function. We need this to be able to run the code.
