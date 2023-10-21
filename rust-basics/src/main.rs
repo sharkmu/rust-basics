303d296159b2cbee66c8a7f47c055e500bbf1597
Samu <csonka.samuel@gmail.com>
2023-10-21 14:57:37 +0200

    input and main function

--- rust-basics/src/main.rs
+++ rust-basics/src/main.rs
@@ -5,3 +5,29 @@ fn hello_world() {
     // print out "Hello World!" in a new line
     println!("Hello World!");
 }
+
+// 2. Input
+
+// import module
+use std::io; 
+fn input() 
+{
+    // mut stand for mutable, which means that we'll be able to change the variable's value later
+    let mut input = String::new(); 
+
+    println!("Please enter some text:");
+
+    // this is an input. We will store the input in the "input" string. ".expect()" handles any upcoming errors
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    // print out the "input" variable
+    println!("You entered: {}", input);
+}
+
+// this is the main function. We need this to be able to run the code.
+fn main()
+{
+    // call the functions
+    hello_world();
+    input();
+}
